70ed1562b768d3d488f29d3646830fa448636d63
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-04-26 21:50:57 +0200

    Create emotions.py

---
+++ PoCs/emotions.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 16:26:49 2015
+
+@author: SoyGema
+"""
+#CODING PARROT EMOTIONS 
+import pybrain
+from pybrain.rl.learners import Q,SARSA
+rl is important for emotions=1
+if rl is important mechanic for emotions:
+    print("reward and punishment is different for every occassion")
+    #RL is a system based on reward and punishment until it gets a politic
+    
+enviroment = emotions
+agent = amygdala
+
+emotion is learnable=1
+if emotion is learnable:
+    print(')
+pi= 3,14
+e= 2,78
+pi/e
+
+pi*e
+pi*e+2
+
+reward= 1
+punishment= -1
+reward*punishment
+
+#defining strings LOVE
+Primary emotion = love 
+Secondary emotion = ((affection, lust, longing))
+Terciary emotion affection = (adoration, fondness, sentimentality, liking, compassion, attractiveness, caring, tenderness)
+Terciary emotion lust = (Desire, passion, Infatuation)
+Terciary emotion longing = longing
+
+def love():
+    Primary emotion
+   for Parrot is affection or lust or longing
+   for Plutchik is joy + trust
+    cos(x)
+    sen(x)
+    e*x
+Is_love = []
+
+#defining strings JOY
+Primary emotion = joy
+secondary emotion = ((cheerfulness, zest, contentment, pride, optimism, enthraliment, relief))
+cheerfulness = ((amusement, bliss, glee, delight, happiness))
+zest = ((enthusiasm, zeal, exicement))
+contentment = (pleasure)
+pride = (triumph)
+optimism = (eargeness, hope)
+entraliment = (rapture)
+relief = (relief)
